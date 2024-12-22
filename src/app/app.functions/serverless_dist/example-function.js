93ca92e16f7ed197099204f2e12d8f96d69ead20
irackson <inasusr@gmail.com>
2024-12-21 22:16:51 -0500

    rm unused js file

--- src/app/app.functions/serverless_dist/example-function.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.main = void 0;
-const main = async (context) => {
-    const { text } = context.parameters;
-    const response = `fTxhis is definitely coming from a serverless function! You entered: ${text}`;
-    return response;
-};
-exports.main = main;
